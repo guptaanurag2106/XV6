7fd9f236af7f6f2c71234f1f25a85adebb921fd6
deepBisman <bismandeepsignh0012@gmail.com>
2024-04-07 22:52:32 +0530

    Modify test scripts

--- a2/run_test.sh
+++ a2/run_test.sh
@@ -1,7 +1,6 @@
-tar czvf assignment2_easy_2020EE10583_2020EE10589.tar.gz -C xv6 .
+tar czvf assignment2_easy_2020EE10583_2020EE10589.tar.gz -C xv6 . > /dev/null 2>&1
 mkdir check_scripts
-tar xzvf check_scripts.tar.gz -C check_scripts
-cp assignment2_easy_2020EE10583_2020EE10589.tar.gz check_scripts
+tar xzvf check_scripts.tar.gz -C check_scripts > /dev/null 2>&1
+cp assignment2_easy_2020EE10583_2020EE10589.tar.gz check_scripts > /dev/null 2>&1
 cd check_scripts
 bash check.sh assignment2_easy_2020EE10583_2020EE10589.tar.gz
- 
